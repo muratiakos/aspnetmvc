534b9a56a6e2a9dc70528377503230d3144edd35
Akos Murati <akos@murati.hu>
2016-04-20 22:12:04 +1200

    Rename aspnetmvc to aspnetmvc2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# aspnetmvc - choco package
+# aspnetmvc2 - choco package
 
 This repository is a choco package project to install and uninstall ASP.NET MVC
 via chocolatey in an unattended manner.
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop';
 
-$packageName = 'aspnetmvc'
+$packageName = 'aspnetmvc2'
 $installerType = 'MSI'
-$url = 'https://download.microsoft.com/download/7/8/C/78C01A05-3B3F-4963-88EC-322BD8E3CBC2/AspNetMVC2.msi'
+$url = 'https://download.microsoft.com/download/7/8/C/78C01A05-3B3F-4963-88EC-322BD8E3CBC2/aspnetmvc2.msi'
 $silentArgs = '/qn'
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
