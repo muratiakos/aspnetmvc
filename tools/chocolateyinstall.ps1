3bd24d00332ddc7d0206ad8f2973540b14471c08
Kierran McPherson <kierranm@gmail.com>
2021-03-03 08:27:24 +1300

    Updating the download URL

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,21 @@
 $ErrorActionPreference = 'Stop';
 
+$url = 'https://download.microsoft.com/download/7/B/1/7B11DE4E-0247-448E-8D39-7C9B12ABA1FF/AspNetMVC2_VS2008.exe'
+
+# Download from a file share
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$downloadLocation = "$toolsDir\AspNetMVC2_VS2008.exe"
+Get-ChocolateyWebFile -PackageName 'AspNetMVC2_VS2008' -FileFullPath "$downloadLocation" -Url "$url"
+
+$extractLocation = "$toolsDir\extracted"
+&"$downloadLocation" /q /x:"$extractLocation"
+
 $packageName = 'aspnetmvc2'
-$installerType = 'MSI'
-$url = 'https://download.microsoft.com/download/7/8/C/78C01A05-3B3F-4963-88EC-322BD8E3CBC2/aspnetmvc2.msi'
+$installerType = 'msi'
+
 $silentArgs = '/qn'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
+Install-ChocolateyInstallPackage -PackageName "$packageName" -FileType "$installerType" -SilentArgs "$silentArgs" -File "$extractLocation\mvcruntime\aspnetmvc2.msi"
+
+Remove-Item -Force -Recurse "$toolsDir\extracted"
+Remove-Item -Force "$downloadLocation"
